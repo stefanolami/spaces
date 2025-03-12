c8a67c7dbc48dd8631c6cf4e084ef960a43717fe
stefanolami <stefanolami90@gmail.com>
2025-03-12 19:26:00 +0200

    fix: hero stretching problem

--- src/components/home/hero-home.tsx
+++ src/components/home/hero-home.tsx
@@ -4,7 +4,7 @@ const HeroHome = () => {
 	return (
 		<div>
 			<div className="bg-[#F3F3F3] w-full lg:h-[calc(100vh_-_120px)] py-8 md:py-6 xl:py-4">
-				<div className="w-[90%] lg:w-auto lg:h-full aspect-[227/146] mx-auto max-w-[1000px] relative">
+				<div className="w-[90%] lg:w-auto lg:h-full aspect-[227/146] mx-auto relative">
 					<Image
 						src="/group-79.png"
 						alt="hero-home"
