5dab72506aeeb8f0320d330426e432434a1b061f
stefanolami <stefanolami90@gmail.com>
2025-06-10 13:45:14 +0300

    chore: add openGraph to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,22 @@ import Footer from '@/components/footer'
 export const metadata: Metadata = {
 	title: 'Time&Spaces',
 	description: '',
+	openGraph: {
+		title: 'Time&Spaces',
+		description:
+			'Your VIEW. Your ACCESS. Your VOICE. Your PLACE OF ACTION.',
+		url: 'https://www.timeandspaces.org',
+		siteName: 'Time&Spaces',
+		images: [
+			{
+				url: 'https://www.timeandspaces.org/web-app-manifest-512x512.png', // Must be an absolute URL
+				width: 512,
+				height: 512,
+			},
+		],
+		locale: 'en_US',
+		type: 'website',
+	},
 }
 
 export default function RootLayout({
